94f2c1c54d31a39dd6b57b13d06cdec11db77da4
Davaeth <masterbelki@gmail.com>
2019-09-02 20:29:07 +0200

    Created home page. Added daily recipe card. Added new recipes cards list. Added BottomAppBar. Created reusable classes and widgets. Updated .gitignore file.

---
+++ lib/components/colors/default_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class DefaultColors {
+  static const backgroundColor = const Color(0xFF141517);
+  static const secondaryColor = const Color(0xFF1E2629);
+  static const iconColor = const Color(0xFFee801e);
+}
